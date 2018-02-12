74679a1322f16cc2bf0cf2e4097a7deebd7696ad
Malik <mi14aaw@herts.ac.uk>
2018-02-12 14:21:28 +0000

    Add files via upload

---
+++ Calculator_Swift/Calculator_Swift/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Calculator_Swift
+//
+//  Created by mi14aaw on 08/02/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+let A = 1
+let B = 2
+var C = (A+B)
+
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mathDisplay: UILabel!
+    
+
+    
+    @IBAction func equalAction(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func addAction(_ sender: Any) {
+    }
+    
+    @IBAction func oneButton(_ sender: Any) {
+    }
+
+    @IBAction func twoButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
